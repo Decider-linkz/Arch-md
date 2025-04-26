928383be4507ba36db15927c1eaff3e2a3f8e852
Decider-linkz <deciderlinkz@gmail.com>
2025-04-26 09:10:30 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ require('dotenv').config();
 
 //================= { SETTINGS } =================\\
 global.prefix = process.env.PREFIX || "."; // your desired prefix symbol only
-global.owner = process.env.OWNER_NUMBER || "2347036214381"; // owner number 
+global.owner = process.env.OWNER_NUMBER || "233505442465"; // owner number 
 global.sudo = process.env.SUDO  || " ";
-global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
+global.ownername = process.env.OWNER_NAME || "deciderboy"; //set bot owner name here 
 global.botname = process.env.BOT_NAME || "αrch md";
 global.author = process.env.AUTHOR  || "Ednut";
 global.packname = process.env.PACK_NAME  || "Arch Md ²⁵";
@@ -20,7 +20,7 @@ global.thumb = process.env.THUMB_IMAGE || 'https://files.catbox.moe/1uhvld.jpg';
 global.footer = process.env.FOOTER || '🤖 © wa bot';
 global.onlypc = process.env.ONLYPC_MSG  || "Sorry buddy the bot won't work directly in group chat to reduce spam use in private chat";
 global.onlygc = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work directly in private chat to reduce spam use in group";
-global.session = process.env.SESSION_ID || "";
+global.session = process.env.SESSION_ID || "Arch_5c80c7c2a04f00dda26fa5eb7de4d2f1";
 global.simbol = process.env.SYMBOL || '♘';
 global.menutype = process.env.MENU_TYPE  || "v2";
 global.warn = process.env.WARN || "3";
